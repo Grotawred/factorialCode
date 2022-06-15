016694c2dadfb2dee10be0cb8e8b054faa59bf20
Oleh Shvaiko <66371852+Vinnialf@users.noreply.github.com>
2022-06-15 21:40:16 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         A(11);
+// Ніколи не називай так методи. В Java є свої правила назв і за цим дуже прискіпливо спостерігають. Привчайся робити правильно. Програма має самодокументуватися!
+
     }
     public static void A(int i){
         int factorial = 1;
